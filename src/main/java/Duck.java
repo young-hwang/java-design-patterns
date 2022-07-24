0ca98b341bbc68c07baf0b1f13ae20f3d547c65a
Young Hwang <ykcul02@gmail.com>
2022-07-24 20:31:52 +0900

    Setting behavior dynamically

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -2,6 +2,14 @@ public abstract class Duck {
     FlyBehavior flyBehavior;
     QuackBehavior quackBehavior;
 
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
+
     public void swim() {
         System.out.println("All ducks float, even decoys!");
     }
--- src/main/java/MiniDuckSimulator.java
+++ src/main/java/MiniDuckSimulator.java
@@ -1,7 +1,12 @@
 public class MiniDuckSimulator {
     public static void main(String[] args) {
         Duck mallard = new MallardDuck();
-        mallard.performFly();
         mallard.performQuack();
+        mallard.performFly();
+
+        Duck model = new ModelDuck();
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
     }
 }
