7e837c73267dbd995cab2da901f7c8d3dae3d98e
Young Hwang <ykcul02@gmail.com>
2022-08-17 23:03:12 +0900

    Tools for your Design Toolbox

--- src/main/java/io/younghwang/javadesignpattern/starbuzz/Beverage.java
+++ src/main/java/io/younghwang/javadesignpattern/starbuzz/Beverage.java
@@ -3,9 +3,19 @@ package io.younghwang.javadesignpattern.starbuzz;
 public abstract class Beverage {
     String description = "Unknown Beverage";
 
+    public enum Size {
+     TALL, GRANDE, VENTI;
+    }
+
+    Size size;
+
     public String getDescription() {
         return description;
     }
 
     public abstract double cost();
+
+    public Size getSize() {
+        return size;
+    }
 }
--- src/main/java/io/younghwang/javadesignpattern/starbuzz/Soy.java
+++ src/main/java/io/younghwang/javadesignpattern/starbuzz/Soy.java
@@ -9,7 +9,15 @@ public class Soy extends CondimentDecorator {
 
     @Override
     public double cost() {
-        return beverage.cost() + 0.15;
+        double cost = beverage.cost();
+        if (beverage.getSize() == Size.TALL) {
+            cost += .10;
+        } else if (beverage.getSize() == Size.GRANDE) {
+            cost += .15;
+        } else if (beverage.getSize() == Size.VENTI) {
+            cost += .20;
+        }
+        return cost;
     }
 
     @Override
