e3977110af45f94ad9cab709459c3cbb9ceb171d
Young Hwang <ykcul02@gmail.com>
2022-09-10 22:56:29 +0900

    Reworking the PizzaStore class

--- src/main/java/io/younghwang/javadesignpattern/simplepizza/Pizza.java
+++ src/main/java/io/younghwang/javadesignpattern/simplepizza/Pizza.java
@@ -1,4 +1,15 @@
 package io.younghwang.javadesignpattern.simplepizza;
 
 public class Pizza {
+    public void prepare() {
+    }
+
+    public void bake() {
+    }
+
+    public void cut() {
+    }
+
+    public void box() {
+    }
 }
