0a8cb272f77b31288bbf0d5a4e7e17101616e8a7
Young Hwang <ykcul02@gmail.com>
2022-10-09 21:36:33 +0900

    We’re just missing one thing: PIZZA!

--- src/main/java/io/younghwang/javadesignpattern/simplepizza/ChicagoStyleCheesePizza.java
+++ src/main/java/io/younghwang/javadesignpattern/simplepizza/ChicagoStyleCheesePizza.java
@@ -1,4 +1,14 @@
 package io.younghwang.javadesignpattern.simplepizza;
 
 public class ChicagoStyleCheesePizza extends Pizza {
+    public ChicagoStyleCheesePizza() {
+        name = "Chicago Style Deep Dish Cheese Pizza";
+        dough = "Extra Thick Crust Dough";
+        sauce = "Plum tomato Sauce";
+        toppings.add("Shredded Mozzarella Cheese");
+    }
+
+    public void cut() {
+        System.out.println("Cutting the pizza into square slices");
+    }
 }
