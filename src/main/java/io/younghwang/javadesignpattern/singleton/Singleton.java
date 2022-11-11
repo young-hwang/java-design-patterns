7ca3ff1d9c92bcc796253ce89ea4e3403d5b7d50
Young Hwang <ykcul02@gmail.com>
2022-11-11 23:52:39 +0900

    Dissecting the classic Singleton Pattern implementation

---
+++ src/main/java/io/younghwang/javadesignpattern/singleton/Singleton.java
@@ -0,0 +1,15 @@
+package io.younghwang.javadesignpattern.singleton;
+
+public class Singleton {
+    private static Singleton uniqueInstance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+}
