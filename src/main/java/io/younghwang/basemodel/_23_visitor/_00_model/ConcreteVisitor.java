75258107acecfd9741d96a469fca8fa16c2ec174
Young Hwang <ykcul02@gmail.com>
2023-04-13 23:23:28 +0900

    주석 제거

--- src/main/java/io/younghwang/basemodel/_23_visitor/_00_model/ConcreteVisitor.java
+++ src/main/java/io/younghwang/basemodel/_23_visitor/_00_model/ConcreteVisitor.java
@@ -1,9 +1,5 @@
 package io.younghwang.basemodel._23_visitor._00_model;
 
-/**
- * 기존 코드를 변경하지 않고 새로운 기능을 추가하는 방법이다.
- * Double Dispatch 를 활용 할 수 있다.
- */
 public class ConcreteVisitor implements Visitor {
     @Override
     public void visit(ElementA elementA) {
--- src/main/java/io/younghwang/basemodel/_23_visitor/_02_after/Client.java
+++ src/main/java/io/younghwang/basemodel/_23_visitor/_02_after/Client.java
@@ -4,11 +4,6 @@ package io.younghwang.basemodel._23_visitor._02_after;
  * Visitor 패턴을 이용하여 수정
  * device는 visitor
  * shape는 element
- * 장접
- * element의 변경 없이 device 추가 가능
- * 단점
- * 구조가 복잡(double dispatch 이해)
- * device의 static한 타입으로 element 추가 변경 시 많은 변경이 필요
  */
 public class Client {
     public static void main(String[] args) {
