c951d894a586ca75db7ff66d99c5b9d5e46e7567
Young Hwang <ykcul02@gmail.com>
2023-04-18 23:38:14 +0900

    Template Method Pattern

---
+++ src/main/java/io/younghwang/basemodel/_22_template_method/_02_after/Client.java
@@ -0,0 +1,11 @@
+package io.younghwang.basemodel._22_template_method._02_after;
+
+public class Client {
+    public static void main(String[] args) {
+        FileProcessor fileProcessor = new FileProcessor("number.txt");
+        int result = fileProcessor.process((sum, number) -> sum + number);
+        System.out.println(result);
+        int result1 = fileProcessor.process((sum, number) -> sum * number);
+        System.out.println(result1);
+    }
+}
