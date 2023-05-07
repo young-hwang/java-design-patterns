25a6a1384093fa0a636c202d63cd23a24eda2aaf
Young Hwang <ykcul02@gmail.com>
2023-05-07 15:46:59 +0900

    observer pattern model

---
+++ src/main/java/io/younghwang/basemodel/_19_observer/_00_model/ConcreteObserver.java
@@ -0,0 +1,8 @@
+package io.younghwang.basemodel._19_observer._00_model;
+
+public class ConcreteObserver implements Observer {
+    @Override
+    public void update() {
+
+    }
+}
---
+++ src/main/java/io/younghwang/basemodel/_19_observer/_00_model/Observer.java
@@ -0,0 +1,5 @@
+package io.younghwang.basemodel._19_observer._00_model;
+
+public interface Observer {
+    void update();
+}
