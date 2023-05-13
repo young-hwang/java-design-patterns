4b0f7851b2df6fcc003862ea1c1c1a4e5dc605df
Young Hwang <ykcul02@gmail.com>
2023-05-14 02:33:22 +0900

    iterator pattern before

---
+++ src/main/java/io/younghwang/basemodel/_16_iterator/_01_before/Client.java
@@ -0,0 +1,27 @@
+package io.younghwang.basemodel._16_iterator._01_before;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Client {
+    public static void main(String[] args) {
+        Board board = new Board();
+        board.addPost("design patter");
+        board.addPost("algorithm");
+        board.addPost("data structure");
+
+        // 들어간 순서대로 순회
+        List<Post> posts = board.getPosts();
+        for (int i = 0; i < posts.size(); i++) {
+            Post post = posts.get(i);
+            System.out.println(post.getTitle());
+        }
+
+        // 가장 최신 글 먼저 순회
+        Collections.sort(posts, (p1, p2) -> p2.getCreatedDateTime().compareTo(p1.getCreatedDateTime()));
+        for (int i = 0; i < posts.size(); i++) {
+            Post post = posts.get(i);
+            System.out.println(post.getTitle());
+        }
+    }
+}
---
+++ src/main/java/io/younghwang/basemodel/_16_iterator/_01_before/Post.java
@@ -0,0 +1,21 @@
+package io.younghwang.basemodel._16_iterator._01_before;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private String title;
+    private LocalDateTime createdDateTime;
+
+    public Post(String title) {
+        this.title = title;
+        this.createdDateTime = LocalDateTime.now();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+}
