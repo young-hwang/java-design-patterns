906259433309b9e1dede1df7950657f42fc70bfd
Young Hwang <ykcul02@gmail.com>
2023-05-20 22:44:33 +0900

    interpreter pattern in java

--- src/main/java/io/younghwang/basemodel/_15_interpreter/_04_java/InterpreterInJava.java
+++ src/main/java/io/younghwang/basemodel/_15_interpreter/_04_java/InterpreterInJava.java
@@ -6,5 +6,8 @@ public class InterpreterInJava {
     public static void main(String[] args) {
         System.out.println(Pattern.matches(".pr...", "spring"));
         System.out.println(Pattern.matches("[a-z]{6}", "spring"));
+        System.out.println(Pattern.matches("you[a-z]{2}[0-9]{4}", "young1984"));
+        System.out.println(Pattern.matches("\\d", "1"));
+        System.out.println(Pattern.matches("\\D", "a"));
     }
 }
