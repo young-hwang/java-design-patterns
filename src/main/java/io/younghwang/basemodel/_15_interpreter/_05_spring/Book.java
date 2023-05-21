9d7824b070715e0004ee408dd98ecacb17b5aac7
Young Hwang <ykcul02@gmail.com>
2023-05-21 18:23:08 +0900

    interpreter pattern in spring

---
+++ src/main/java/io/younghwang/basemodel/_15_interpreter/_05_spring/Book.java
@@ -0,0 +1,17 @@
+package io.younghwang.basemodel._15_interpreter._05_spring;
+
+public class Book {
+    private String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
