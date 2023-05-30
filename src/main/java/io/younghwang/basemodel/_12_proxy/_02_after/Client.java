015f8a495d79c047255133af96c0c1d9cf391a4c
Young Hwang <ykcul02@gmail.com>
2023-05-30 23:59:07 +0900

    proxy pattern after

---
+++ src/main/java/io/younghwang/basemodel/_12_proxy/_02_after/Client.java
@@ -0,0 +1,8 @@
+package io.younghwang.basemodel._12_proxy._02_after;
+
+public class Client {
+    public static void main(String[] args) {
+        GameService gameService = new GameServiceProxy();
+        gameService.startGame();
+    }
+}
