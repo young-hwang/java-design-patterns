fc6480ffa7ef8589fe513b01404d1bbffca2e032
Young Hwang <ykcul02@gmail.com>
2023-06-01 22:29:44 +0900

    proxy pattern in java

---
+++ src/main/java/io/younghwang/basemodel/_12_proxy/_04_java/GameService.java
@@ -0,0 +1,5 @@
+package io.younghwang.basemodel._12_proxy._04_java;
+
+public interface GameService {
+    void startGame();
+}
---
+++ src/main/java/io/younghwang/basemodel/_12_proxy/_04_java/GameServiceReal.java
@@ -0,0 +1,12 @@
+package io.younghwang.basemodel._12_proxy._04_java;
+
+public class GameServiceReal implements GameService {
+    public void startGame() {
+        System.out.println("게임을 시작합니다.");
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
