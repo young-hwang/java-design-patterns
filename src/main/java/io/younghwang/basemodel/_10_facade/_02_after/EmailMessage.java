d4d0ad9a1e2baf00d54cc7cfe9c53e2da50b4c20
Young Hwang <ykcul02@gmail.com>
2023-06-06 23:18:23 +0900

    facade pattern after

---
+++ src/main/java/io/younghwang/basemodel/_10_facade/_02_after/EmailMessage.java
@@ -0,0 +1,22 @@
+package io.younghwang.basemodel._10_facade._02_after;
+
+public class EmailMessage {
+    private String subject;
+    private String text;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
