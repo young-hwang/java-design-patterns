5a078fd98c3eda3ea0a55a58d68d5e4a30d7a2d9
Young Hwang <ykcul02@gmail.com>
2023-06-15 00:45:15 +0900

    adapter pattern model

---
+++ src/main/java/io/younghwang/basemodel/_06_adapter/_00_model/Adaptee.java
@@ -0,0 +1,7 @@
+package io.younghwang.basemodel._06_adapter._00_model;
+
+public class Adaptee {
+    public void specificOperation() {
+
+    }
+}
---
+++ src/main/java/io/younghwang/basemodel/_06_adapter/_00_model/Target.java
@@ -0,0 +1,5 @@
+package io.younghwang.basemodel._06_adapter._00_model;
+
+public interface Target {
+    void operation();
+}
