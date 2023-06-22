9fae21e35437512f6f9d463553e00d0fc47451a7
Young Hwang <ykcul02@gmail.com>
2023-06-23 00:49:07 +0900

    abstract factory pattern in java

---
+++ src/main/java/io/younghwang/basemodel/_03_abstract_factory/_03_java/AbstractFactoryInJava.java
@@ -0,0 +1,19 @@
+package io.younghwang.basemodel._03_abstract_factory._03_java;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+public class AbstractFactoryInJava {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document parse = documentBuilder.parse(new File("Book.xml"));
+        System.out.println(parse.getDocumentElement());
+    }
+}
