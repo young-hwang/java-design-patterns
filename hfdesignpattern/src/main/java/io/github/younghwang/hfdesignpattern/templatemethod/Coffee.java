433139fcf8c16a73ed65654a5f40bc2f023a544c
Young Hwang <ykcul02@gmail.com>
2023-08-05 22:33:17 +0900

    Whipping up some coffee and tea classes

---
+++ hfdesignpattern/src/main/java/io/github/younghwang/hfdesignpattern/templatemethod/Coffee.java
@@ -0,0 +1,26 @@
+package io.github.younghwang.hfdesignpattern.templatemethod;
+
+public class Coffee {
+    public void prepareReceipt() {
+        boilWater();
+        brewCoffeeGrinds();
+        pourInCup();
+        addSugarAndMild();
+    }
+
+    private void addSugarAndMild() {
+        System.out.println("Adding Sugar and Milk");
+    }
+
+    private void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+
+    private void brewCoffeeGrinds() {
+        System.out.println("Dripping Coffee through filter");
+    }
+
+    private void boilWater() {
+        System.out.println("Boiling water");
+    }
+}
---
+++ hfdesignpattern/src/main/java/io/github/younghwang/hfdesignpattern/templatemethod/Tea.java
@@ -0,0 +1,27 @@
+package io.github.younghwang.hfdesignpattern.templatemethod;
+
+public class Tea {
+    public void prepareReceipt() {
+        boilWater();
+        steepTeaBag();
+        pourInCup();
+        addSugarAndMild();
+    }
+
+    private void addSugarAndMild() {
+        System.out.println("Adding Sugar and Milk");
+    }
+
+    private void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+
+    private void steepTeaBag() {
+        System.out.println("Steeping the tea");
+    }
+
+    private void boilWater() {
+        System.out.println("Boiling water");
+    }
+}
+}
