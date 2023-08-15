2a0c2cd04155699efa0643b23764001ea23b0117
Young Hwang <ykcul02@gmail.com>
2023-08-15 22:47:20 +0900

    Is the Waitress ready for prime time?

--- hfdesignpattern/src/main/java/io/github/younghwang/hfdesignpattern/iterator/MenuTestDrive.java
+++ hfdesignpattern/src/main/java/io/github/younghwang/hfdesignpattern/iterator/MenuTestDrive.java
@@ -1,13 +1,19 @@
 package io.github.younghwang.hfdesignpattern.iterator;
 
+import java.util.ArrayList;
+
 public class MenuTestDrive {
     public static void main(String[] args) {
         PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
         DinerMenu dinerMenu = new DinerMenu();
         CafeMenu cafeMenu = new CafeMenu();
 
-        Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu, cafeMenu);
+        ArrayList<Menu> menus = new ArrayList<>();
+        menus.add(pancakeHouseMenu);
+        menus.add(dinerMenu);
+        menus.add(cafeMenu);
+        Waitress waitress = new Waitress(menus);
 
-        waitress.printMen();
+        waitress.printMenu();
     }
 }
--- hfdesignpattern/src/main/java/io/github/younghwang/hfdesignpattern/iterator/Waitress.java
+++ hfdesignpattern/src/main/java/io/github/younghwang/hfdesignpattern/iterator/Waitress.java
@@ -1,44 +1,38 @@
 package io.github.younghwang.hfdesignpattern.iterator;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Waitress {
-    Menu pancakeHouseMenu;
-    Menu dinerMenu;
-    Menu cafeMenu;
+    ArrayList<Menu> menus;
+//    Menu pancakeHouseMenu;
+//    Menu dinerMenu;
+//    Menu cafeMenu;
 
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
-        this.pancakeHouseMenu = pancakeHouseMenu;
-        this.dinerMenu = dinerMenu;
-        this.cafeMenu = cafeMenu;
+    public Waitress(ArrayList<Menu> menus) {
+        this.menus = menus;
     }
 
-    void printMen() {
-        Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
-        Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
-        Iterator<MenuItem> cafeIterator = cafeMenu.createIterator();
-        System.out.println("MENU - BREAKFAST");
-        printMenu(pancakeIterator);
-        System.out.println("MENU - LUNCH");
-        printMenu(dinerIterator);
-        System.out.println("MENU - DINNER");
-        printMenu(cafeIterator);
-    }
+//    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
+//        this.pancakeHouseMenu = pancakeHouseMenu;
+//        this.dinerMenu = dinerMenu;
+//        this.cafeMenu = cafeMenu;
+//    }
 
-    void printBreakfastMenu() {
-
-    }
-
-    void printLunchMenu() {
-
-    }
-
-    void printVegetarianMenu() {
-
-    }
-
-    boolean isItemVegetarian(String name) {
-        return true;
+    void printMenu() {
+        Iterator<Menu> menuIterator = menus.iterator();
+        while (menuIterator.hasNext()) {
+            printMenu(menuIterator.next().createIterator());
+        }
+//        Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
+//        Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
+//        Iterator<MenuItem> cafeIterator = cafeMenu.createIterator();
+//        System.out.println("MENU - BREAKFAST");
+//        printMenu(pancakeIterator);
+//        System.out.println("MENU - LUNCH");
+//        printMenu(dinerIterator);
+//        System.out.println("MENU - DINNER");
+//        printMenu(cafeIterator);
     }
 
     private void printMenu(Iterator iterator) {
