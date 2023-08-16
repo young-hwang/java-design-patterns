24e3b1d7a75f21a995fa08994284100df7cd0eb3
Young Hwang <ykcul02@gmail.com>
2023-08-16 22:01:33 +0900

    Implementing the Composite Menu

--- hfdesignpattern/src/main/java/io/github/younghwang/hfdesignpattern/iterator/Menu.java
+++ hfdesignpattern/src/main/java/io/github/younghwang/hfdesignpattern/iterator/Menu.java
@@ -1,7 +1,57 @@
 package io.github.younghwang.hfdesignpattern.iterator;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
-public interface Menu {
-    Iterator<MenuItem> createIterator();
+public class Menu extends MenuComponent {
+    ArrayList<MenuComponent> menuComponents = new ArrayList<>();
+    String name;
+    String description;
+
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public void add(MenuComponent component) {
+        this.menuComponents.add(component);
+    }
+
+    @Override
+    public void remove(MenuComponent component) {
+        this.menuComponents.remove(component);
+    }
+
+    @Override
+    public MenuComponent getChild(int i) {
+        return this.menuComponents.get(i);
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public void print() {
+        System.out.println(this.toString());
+        Iterator<MenuComponent> iterator = menuComponents.iterator();
+        while (iterator.hasNext()) {
+            iterator.next().print();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
