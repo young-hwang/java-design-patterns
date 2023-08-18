b9347947f88b07c309e208ec665b30bd6140a532
Young Hwang <ykcul02@gmail.com>
2023-08-18 22:23:01 +0900

    The Null Iterator

---
+++ hfdesignpattern/src/main/java/io/github/younghwang/hfdesignpattern/iterator/NullIterator.java
@@ -0,0 +1,20 @@
+package io.github.younghwang.hfdesignpattern.iterator;
+
+import java.util.Iterator;
+
+public class NullIterator implements Iterator<MenuComponent> {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public MenuComponent next() {
+        return null;
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+}
