be8a16b132332d746c0f3a0f84778d5966da188b
Young Hwang <ykcul02@gmail.com>
2023-09-04 23:12:51 +0900

    Testing the matchmaking service

--- hfdesignpattern/src/main/java/io/github/younghwang/hfdesignpattern/proxy/dynamic/MatchMakingTestDrive.java
+++ hfdesignpattern/src/main/java/io/github/younghwang/hfdesignpattern/proxy/dynamic/MatchMakingTestDrive.java
@@ -1,12 +1,60 @@
 package io.github.younghwang.hfdesignpattern.proxy.dynamic;
 
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MatchMakingTestDrive {
+    private Map<String, PersonBean> personBeanMap = new HashMap<>();
+
     public static void main(String[] args) {
         MatchMakingTestDrive matchMakingTestDrive = new MatchMakingTestDrive();
-        PersonBeanImpl personBean = new PersonBeanImpl();
-        PersonBean ownerProxy = matchMakingTestDrive.getOwnerProxy(personBean);
+        matchMakingTestDrive.drive();
+    }
+
+    private void drive() {
+        PersonBean joe = getPersonFromDatabase("Joe Javabean");
+
+        PersonBean ownerProxy = this.getOwnerProxy(joe);
+        System.out.println("Name is " + ownerProxy.getName());
+        ownerProxy.setInterests("Bowling, Golf");
+        System.out.println("Interests set from owner proxy");
+        try {
+            ownerProxy.setHotOrNotRating(10);
+        } catch (Exception e) {
+            System.out.println("Can't set rating from owner proxy");
+        }
+        System.out.println("Rating is " + ownerProxy.getHotOrNotRating());
+
+        PersonBean nonOwnerProxy = this.getNonOwnerProxy(joe);
+        System.out.println("Name is " + nonOwnerProxy.getName());
+        try {
+            nonOwnerProxy.setInterests("Bowing, Golf");
+        } catch (Exception e) {
+            System.out.println("Can't set interests from non owner proxy");
+        }
+        nonOwnerProxy.setHotOrNotRating(3);
+        System.out.println("Rating set from non owner proxy");
+        System.out.println("Rating is " + nonOwnerProxy.getHotOrNotRating());
+    }
+
+    private PersonBean getPersonFromDatabase(String name) {
+        return personBeanMap.get(name);
+    }
+
+    public MatchMakingTestDrive() {
+        initializeDatabase();
+    }
+
+    private void initializeDatabase() {
+        PersonBeanImpl joe = new PersonBeanImpl();
+        joe.setName("Joe Javabean");
+        joe.setGender("Male");
+        joe.setInterests("Java");
+        joe.setHotOrNotRating(5);
+        personBeanMap.put(joe.getName(), joe);
     }
 
     PersonBean getOwnerProxy(PersonBean personBean) {
@@ -14,5 +62,12 @@ public class MatchMakingTestDrive {
                 personBean.getClass().getClassLoader(),
                 personBean.getClass().getInterfaces(),
                 new OwnerInvocationHandler(personBean));
+    }
+
+    PersonBean getNonOwnerProxy(PersonBean personBean) {
+        return (PersonBean) Proxy.newProxyInstance(
+                personBean.getClass().getClassLoader(),
+                personBean.getClass().getInterfaces(),
+                new NonOwnerInvocationHandler(personBean));
     }
 }
