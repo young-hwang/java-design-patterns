dd5a02bf4eb444522d91379432bc1a9ae8e88320
young-hwang <ykcul02@gmail.com>
2025-01-13 00:00:21 +0900

    add: robot - 1

---
+++ src/main/java/me/_21_strategy/java/robot/_01_before/Command.java
@@ -0,0 +1,36 @@
+package me._21_strategy.java.robot._01_before;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Command {
+    public static final Command FORWARD = new Command("forward");
+    public static final Command BACKWARD = new Command("backward");
+    public static final Command TURN_RIGHT = new Command("right");
+    public static final Command TURN_LEFT = new Command("left");
+
+    private final String name;
+    private static final Map<String, Command> commands = new HashMap<>();
+
+    public Command(String name) {
+        this.name = name;
+    }
+
+    static {
+        commands.put(FORWARD.name, FORWARD);
+        commands.put(BACKWARD.name, BACKWARD);
+        commands.put(TURN_RIGHT.name, TURN_RIGHT);
+        commands.put(TURN_LEFT.name, TURN_LEFT);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Command parseCommand(String commandString) {
+        if (commands.containsKey(commandString)) {
+            return commands.get(commandString);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/me/_21_strategy/java/robot/_01_before/Position.java
@@ -0,0 +1,24 @@
+package me._21_strategy.java.robot._01_before;
+
+public class Position {
+    public int x;
+    public int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void relativeMove(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
