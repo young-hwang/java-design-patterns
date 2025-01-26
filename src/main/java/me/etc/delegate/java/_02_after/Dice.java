5f93264b17f188a425725ea324767262942e33d2
young-hwang <ykcul02@gmail.com>
2025-01-26 16:42:49 +0900

    add: delegate - 02

---
+++ src/main/java/me/etc/delegate/java/_02_after/Dice.java
@@ -0,0 +1,48 @@
+package me.etc.delegate.java._02_after;
+
+import java.util.Random;
+
+public class Dice extends Random {
+    private final Random random;
+
+    public Dice() {
+        super(314159L);
+        random = this;
+    }
+
+    public Dice(long seed) {
+        super(seed);
+        random = this;
+    }
+
+    @Override
+    public int nextInt() {
+        return random.nextInt(6) + 1;
+    }
+
+
+    @Override
+    public long nextLong() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean nextBoolean() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public float nextFloat() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public double nextDouble() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public synchronized double nextGaussian() {
+        throw new UnsupportedOperationException();
+    }
+}
