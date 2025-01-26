49eb852bb65bbcd2618ec2a97ff625a35f57adc7
young-hwang <ykcul02@gmail.com>
2025-01-26 16:53:59 +0900

    add: delegate - 03

---
+++ src/main/java/me/etc/delegate/java/_03_after/Dice.java
@@ -0,0 +1,23 @@
+package me.etc.delegate.java._03_after;
+
+import java.util.Random;
+
+public class Dice {
+    private final Random random;
+
+    public Dice() {
+        random = new Random(314159L);
+    }
+
+    public Dice(long seed) {
+        random = new Random(seed);
+    }
+
+    public int nextInt() {
+        return random.nextInt(6) + 1;
+    }
+
+    public void setSeed(long seed) {
+        random.setSeed(seed);
+    }
+}
---
+++ src/main/java/me/etc/delegate/java/_03_after/Main.java
@@ -0,0 +1,20 @@
+package me.etc.delegate.java._03_after;
+
+public class Main {
+    public static void main(String[] args) {
+        Dice dice0 = new Dice();
+        Dice dice1 = new Dice(456L);
+        Dice dice2 = new Dice();
+
+        Dice[] dices = {dice0, dice1, dice2};
+
+        dice2.setSeed(456L);
+
+        for (Dice dice : dices) {
+            for (int i = 0; i < 10; i++) {
+                System.out.printf("%d, ", dice.nextInt());
+            }
+            System.out.println();
+        }
+    }
+}
