50ada15a48c4d04cee68c38569c340a1b4874380
young-hwang <ykcul02@gmail.com>
2025-01-27 18:54:31 +0900

    add: hide delegate - 03

---
+++ src/main/java/me/etc/delegate/java/address/_03_after/Database.java
@@ -0,0 +1,47 @@
+package me.etc.delegate.java.address._03_after;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class Database {
+    private final String fileName;
+    private final Path path;
+    private final Properties properties;
+
+    public Database(String fileName) {
+        this.fileName = fileName;
+        this.properties = new Properties();
+        this.path = Path.of(fileName);
+        if (!Files.exists(path)) {
+            try {
+                Files.createFile(path);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try(var file = Files.newInputStream(path)){
+            properties.load(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void set(String key, String value) {
+        properties.setProperty(key, value);
+    }
+
+    public String get(String key) {
+        return properties.getProperty(key, null);
+    }
+
+    public void update() throws IOException {
+        properties.store(Files.newOutputStream(this.path), "");
+    }
+
+    public Enumeration keys() {
+        return properties.propertyNames();
+    }
+}
